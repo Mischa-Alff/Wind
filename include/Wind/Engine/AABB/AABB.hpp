eb45d6fb6636e740f60c7d795401d67aeaaead0a
Mischa Aster Alff <mischa.alff@gmail.com>
2014-11-02 04:05:08 +0100

    Engine: AABB: Made contains and intersects member functions const

--- include/Wind/Engine/AABB/AABB.hpp
+++ include/Wind/Engine/AABB/AABB.hpp
@@ -11,8 +11,8 @@ namespace wind
 		Vector2f center;
 		Vector2f half_size;
 
-		bool contains(const Vector2f &point);
-		bool intersects(const AABB &other);
+		bool contains(const Vector2f &point) const;
+		bool intersects(const AABB &other) const;
 
 		AABB(const Vector2f &center={0.f, 0.f}, const Vector2f &half_size={0.f, 0.f});
 	};
--- src/Wind/Engine/AABB/AABB.cpp
+++ src/Wind/Engine/AABB/AABB.cpp
@@ -8,13 +8,13 @@ namespace wind
 	, half_size{half_size}
 	{}
 
-	bool AABB::contains(const Vector2f &point)
+	bool AABB::contains(const Vector2f &point) const
 	{
 		return (std::abs(center.x-point.x) <= half_size.x) 
 		    && (std::abs(center.y-point.y) <= half_size.y); 
 	}
 
-	bool AABB::intersects(const AABB &other)
+	bool AABB::intersects(const AABB &other) const
 	{
 		return (std::abs(center.x-other.center.x) <= (half_size.x+other.half_size.x))
 			&& (std::abs(center.y-other.center.y) <= (half_size.y+other.half_size.y));
