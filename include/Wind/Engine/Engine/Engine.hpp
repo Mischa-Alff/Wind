143c084da8f20d905f44c0a1c0799ec6d2af014e
Mischa Aster Alff <mischa.alff@gmail.com>
2014-11-02 13:52:34 +0100

    Engine: Engine: Split simulation into multiple functions

--- include/Wind/Engine/Engine/Engine.hpp
+++ include/Wind/Engine/Engine/Engine.hpp
@@ -16,13 +16,26 @@ namespace wind
 	private:
 		unsigned int m_id_counter;
 		std::vector<std::shared_ptr<Entity>> m_entities;
+
+		void apply_gravity();
+		void apply_gravity(std::shared_ptr<Entity> &a, bool both=false);
+		void apply_gravity(std::shared_ptr<Entity> &a, std::vector<std::shared_ptr<Entity>>::iterator begin, bool both=false);
+		void apply_gravity(std::shared_ptr<Entity> &a, std::shared_ptr<Entity> &b);
+
+		void apply_collisions();
+		void apply_collisions(std::shared_ptr<Entity> &a, std::vector<std::shared_ptr<Entity>>::iterator begin);
+		void apply_collisions(std::shared_ptr<Entity> &a, std::shared_ptr<Entity> &b);
+
+		void integrate(const StandardDuration &deltatime);
+		void integrate(std::shared_ptr<Entity> &entity, const StandardDuration &deltatime);
+
 	public:
 		bool gravity=true;
 
 		void add_entity(std::shared_ptr<Entity> entity);
 		void remove_entity(std::shared_ptr<Entity> entity);
 
-		void simulate(StandardDuration deltatime);
+		void simulate(const StandardDuration deltatime);
 
 		Engine();
 	};
--- src/Wind/Engine/Engine/Engine.cpp
+++ src/Wind/Engine/Engine/Engine.cpp
@@ -16,95 +16,179 @@ namespace wind
 		m_entities.pop_back();
 	}
 
-	void Engine::simulate(StandardDuration deltatime)
+	void Engine::apply_gravity()
 	{
-		for(auto entity_itr=0; entity_itr < m_entities.size(); ++entity_itr)
+		for(auto it_a=m_entities.begin(); it_a != m_entities.end(); ++it_a)
 		{
-			auto& entity = m_entities[entity_itr];
-
-			if(gravity)
+			auto &a = *it_a;
+			if(!a->gravity_affected && !a->gravity_exert)
+				return;
+			if(!a->mass)
+				return;
+			for(auto it_b=it_a+1; it_b != m_entities.end(); ++it_b)
 			{
-				if(!entity->gravity_affected && !entity->gravity_exert)
-					continue;
-				if(!entity->mass)
-					continue;
-				for(auto entity_fund_itr=entity_itr+1; entity_fund_itr < m_entities.size(); ++entity_fund_itr)
-				{
-					auto& entity_fund = m_entities[entity_fund_itr];
-					if(!entity_fund->mass)
-						continue;
-					if(!(  (entity_fund->gravity_exert && entity->gravity_affected) 
-						|| (entity->gravity_exert && entity_fund->gravity_affected)))
-						continue;
+				auto &b = *it_b;
+				apply_gravity(a, b);
+			}
+		}
+	}
+	
+	void Engine::apply_gravity(std::shared_ptr<Entity> &a, bool both)
+	{
+		apply_gravity(a, m_entities.begin(), both);
+	}
 
-					auto distance_vec = entity->position - entity_fund->position;
-					auto distance_pow2 = distance_vec.x*distance_vec.x + distance_vec.y*distance_vec.y;
-
-					float force = G * (entity->mass*entity_fund->mass)/(distance_pow2);
-					Vector2f direction = (entity_fund->position-entity->position) / sqrt(distance_pow2);
-
-					if(entity_fund->gravity_exert && entity->gravity_affected)
-						entity->impulse_force += direction*force;
-
-					if(entity->gravity_exert && entity_fund->gravity_affected)
-						entity_fund->impulse_force += -direction*force;
-				}
+	void Engine::apply_gravity(std::shared_ptr<Entity> &a, std::vector<std::shared_ptr<Entity>>::iterator begin, bool both)
+	{
+		if(!a->gravity_affected && !a->gravity_exert)
+			return;
+		if(!a->mass)
+			return;
+		for(auto it_b = begin; it_b != m_entities.end(); ++it_b)
+		{
+			auto &b = *it_b;
+			if(both)
+			{
+				apply_gravity(a, b);
+				continue;
 			}
 
-			Vector2f acceleration = (entity->force+entity->impulse_force) / entity->mass;
-			entity->velocity += acceleration*deltatime.count();
-			entity->position += entity->velocity*deltatime.count();
-			entity->impulse_force = {0.f, 0.f};
+			if(!b->mass)
+				continue;
+			if(!(b->gravity_exert && a->gravity_affected))
+				continue;
 
-			for(auto entity_col_itr=entity_itr+1; entity_col_itr < m_entities.size(); ++entity_col_itr)
+			auto distance_vec = a->position - b->position;
+			auto distance_pow2 = distance_vec.x*distance_vec.x + distance_vec.y*distance_vec.y;
+
+			float force = G * (a->mass*b->mass)/(distance_pow2);
+			Vector2f direction = (b->position-a->position) / sqrt(distance_pow2);
+
+			if(b->gravity_exert && a->gravity_affected)
+				a->impulse_force += direction*force;
+		}	
+	}
+
+	void Engine::apply_gravity(std::shared_ptr<Entity> &a, std::shared_ptr<Entity> &b)
+	{
+		if((!a->gravity_affected && !a->gravity_exert) || (!b->gravity_affected && !b->gravity_exert))
+			return;
+		if(!(b->gravity_exert && a->gravity_affected) && !(a->gravity_exert && b->gravity_affected))
+			return;
+		if(!a->mass || !b->mass)
+			return;
+
+		auto distance_vec = a->position - b->position;
+		auto distance_pow2 = distance_vec.x*distance_vec.x + distance_vec.y*distance_vec.y;
+
+		float force = G * (a->mass*b->mass)/(distance_pow2);
+		Vector2f direction = (b->position-a->position) / sqrt(distance_pow2);
+
+		if(b->gravity_exert && a->gravity_affected)
+			a->impulse_force += direction*force;
+
+		if(a->gravity_exert && b->gravity_affected)
+			b->impulse_force += -direction*force;
+	}
+
+
+	void Engine::apply_collisions()
+	{
+		for(auto it_a=m_entities.begin(); it_a != m_entities.end(); ++it_a)
+		{
+			auto &a = *it_a;
+			for(auto it_b=it_a+1; it_b != m_entities.end(); ++it_b)
 			{
-				auto& entity_col = m_entities[entity_col_itr];
+				auto &b = *it_b;
+				apply_collisions(a, b);
+			}
+		}
+	}
 
-				for(auto &entity_shape : entity->get_body()->get_shapes())
+	void Engine::apply_collisions(std::shared_ptr<Entity> &a, std::vector<std::shared_ptr<Entity>>::iterator begin)
+	{
+		for(auto it_b = begin; it_b != m_entities.end(); ++it_b)
+		{
+			auto &b = *it_b;
+			apply_collisions(a, b);
+		}
+	}
+
+	void Engine::apply_collisions(std::shared_ptr<Entity> &a, std::shared_ptr<Entity> &b)
+	{
+		if(!(a->get_body() && b->get_body()))
+			return;
+		for(auto &shape_a : a->get_body()->get_shapes())
+		{
+			for(auto &shape_b : b->get_body()->get_shapes())
+			{
+				if(shape_a->get_AABB(a->position).intersects(shape_b->get_AABB(b->position)))
 				{
-					for(auto &entity_col_shape : entity_col->get_body()->get_shapes())
+					if(shape_a->type() == Shape::Circle)
 					{
-						if(entity_shape->get_AABB(entity->position).intersects(entity_col_shape->get_AABB(entity_col->position)))
+						if(shape_b->type() == Shape::Circle)
 						{
-							if(entity_shape->type() == Shape::Circle)
+							std::shared_ptr<CircleShape> ca = std::static_pointer_cast<CircleShape>(shape_a);
+							std::shared_ptr<CircleShape> cb = std::static_pointer_cast<CircleShape>(shape_b);
+
+							Vector2f tmp = a->position - b->position;
+							float tmp_len_pow2 = tmp.x*tmp.x + tmp.y*tmp.y;
+							float sum_rad = ca->radius+cb->radius;
+							float sum_rad_pow2 = sum_rad * sum_rad;
+
+							if(tmp_len_pow2 < sum_rad_pow2)
 							{
-								if(entity_col_shape->type() == Shape::Circle)
-								{
-									std::shared_ptr<CircleShape> c1 = std::static_pointer_cast<CircleShape>(entity_shape);
-									std::shared_ptr<CircleShape> c2 = std::static_pointer_cast<CircleShape>(entity_col_shape);
+								float tmp_len = std::sqrt(tmp_len_pow2);
+								float minimum_translation_distance = sum_rad - tmp_len;
+								float vel_len = std::sqrt(a->velocity.x*a->velocity.x + a->velocity.y*a->velocity.y);
+								Vector2f unit_vel = a->velocity/vel_len;
+								a->position -= unit_vel * minimum_translation_distance;
+								tmp_len_pow2 = sum_rad_pow2;
+							}
 
-									Vector2f tmp = entity->position - entity_col->position;
-									float tmp_len_pow2 = tmp.x*tmp.x + tmp.y*tmp.y;
-									float sum_rad = c1->radius+c2->radius;
-									float sum_rad_pow2 = sum_rad * sum_rad;
+							if(tmp_len_pow2 <= sum_rad_pow2)
+							{
+								Vector2f old_velocity = a->velocity;
+								Vector2f old_velocity_col = b->velocity;
 
-									// if(tmp_len_pow2 < sum_rad_pow2)
-									// {
-									// 	float tmp_len = sqrt(tmp_len_pow2);
-									// 	float minimum_translation_distance = sum_rad - tmp_len;
-									// 	float vel_len = sqrt(entity->velocity.x*entity->velocity.x + entity->velocity.x*entity->velocity.y);
-									// 	Vector2f unit_vel = entity->velocity/vel_len;
-									// 	entity->position -= unit_vel * minimum_translation_distance;
-									// 	tmp_len_pow2 = sum_rad_pow2;
-									// }
+								a->velocity = ((a->mass - b->mass)/(a->mass + b->mass)*old_velocity
+								             + (2.0*b->mass)/(a->mass+b->mass)*old_velocity_col);
 
-									if(tmp_len_pow2 <= sum_rad_pow2)
-									{
-										Vector2f old_velocity = entity->velocity;
-										Vector2f old_velocity_col = entity_col->velocity;
-										entity->velocity = ((entity->mass - entity_col->mass)/(entity->mass + entity_col->mass)*old_velocity
-														  + (2.0*entity_col->mass)/(entity->mass+entity_col->mass)*old_velocity_col);
-
-
-										entity_col->velocity = ((entity_col->mass - entity->mass)/(entity->mass + entity_col->mass)*old_velocity_col
-										                  + (2.0*entity->mass)/(entity->mass+entity_col->mass)*old_velocity);
-									}
-								}
+								b->velocity = ((b->mass - a->mass)/(a->mass + b->mass)*old_velocity_col
+								             + (2.0*a->mass)/(a->mass+b->mass)*old_velocity);
 							}
 						}
 					}
 				}
 			}
+		}
+	}
+
+
+	void Engine::integrate(const StandardDuration &deltatime)
+	{
+		for(auto &a : m_entities)
+			integrate(a, deltatime);
+	}
+
+	void Engine::integrate(std::shared_ptr<Entity> &entity, const StandardDuration &deltatime)
+	{
+		Vector2f acceleration = (entity->force+entity->impulse_force) / entity->mass;
+		entity->velocity += acceleration*deltatime.count();
+		entity->position += entity->velocity*deltatime.count();
+		entity->impulse_force = {0.f, 0.f};
+	}
+
+
+	void Engine::simulate(const StandardDuration deltatime)
+	{
+		for(auto it_a = m_entities.begin(); it_a != m_entities.end(); ++it_a)
+		{
+			auto &a = *it_a;
+			if(gravity)
+				apply_gravity(a, it_a+1, true);
+			integrate(a, deltatime);
+			apply_collisions(a, it_a+1);
 		}
 	}
 
