5896b4ebf2c2d05ba7ec6585023e48fb1f5ce7ee
Mischa Aster Alff <mischa.alff@gmail.com>
2014-11-02 19:47:30 +0100

    Engine: AABB: Added .contains(AABB) method

--- include/Wind/Engine/AABB/AABB.hpp
+++ include/Wind/Engine/AABB/AABB.hpp
@@ -12,6 +12,7 @@ namespace wind
 		Vector2f half_size;
 
 		bool contains(const Vector2f &point) const;
+		bool contains(const AABB &other) const;
 		bool intersects(const AABB &other) const;
 
 		AABB(const Vector2f &center={0.f, 0.f}, const Vector2f &half_size={0.f, 0.f});
--- src/Wind/Engine/AABB/AABB.cpp
+++ src/Wind/Engine/AABB/AABB.cpp
@@ -8,10 +8,17 @@ namespace wind
 	, half_size{half_size}
 	{}
 
-	bool AABB::contains(const Vector2f &point) const
+	inline bool AABB::contains(const Vector2f &point) const
 	{
 		return (std::abs(center.x-point.x) <= half_size.x) 
 		    && (std::abs(center.y-point.y) <= half_size.y); 
+	}
+
+	bool AABB::contains(const AABB &other) const
+	{
+		Vector2f constraint_half_size = half_size-other.half_size;
+		return (std::abs(center.x-other.center.x) <= constraint_half_size.x)
+		    && (std::abs(center.y-other.center.y) <= constraint_half_size.y);
 	}
 
 	bool AABB::intersects(const AABB &other) const
