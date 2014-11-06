893e835b6bdb5a9bb3ec74b2d16cc20818a21a2e
Mischa Aster Alff <mischa.alff@gmail.com>
2014-11-06 23:11:55 +0100

    examples: textbench: Updated testbench to make use of quadtrees

--- examples/testbench/testbench.cpp
+++ examples/testbench/testbench.cpp
@@ -7,6 +7,9 @@
 #include <Wind/Engine/Shape/CircleShape.hpp>
 #include <Wind/Engine/Entity/Entity.hpp>
 #include <Wind/Engine/Engine/Engine.hpp>
+#include <Wind/Engine/Partitioning/QuadTree.hpp>
+
+void draw_quadtree(sf::RenderWindow &window, const wind::QuadTree *tree);
 
 int main()
 {
@@ -18,10 +21,24 @@ int main()
 	particle_drawable.setRadius(0.75f);
 	particle_drawable.setFillColor(sf::Color::Red);
 
+	std::shared_ptr<wind::QuadTree> quadtree = std::make_shared<wind::QuadTree>(
+		wind::QuadTree{{
+			{window.getSize().x/2.f, window.getSize().y/2.f},
+			{window.getSize().x/2.f, window.getSize().y/2.f}
+		}}
+	);
+
+	quadtree->max_depth = 5;
+	quadtree->max_elements_before_split = 10;
+
 	wind::Engine engine;
 
+	bool render_quadtree=true;
+	engine.use_quadtree = true;
+	engine.set_quadtree(quadtree);
+
 	// Create array of physics objects
-	wind::Vector2u amount{40, 40};
+	wind::Vector2u amount{20, 20};
 	std::vector<std::shared_ptr<wind::Entity>> particles;
 	particles.resize(amount.x*amount.y);
 	std::cout<<"num_particles: "<<particles.size()<<std::endl;
@@ -37,10 +54,10 @@ int main()
 		auto& particle = particles[particle_index];
 		particle = std::make_shared<wind::Entity>();
 
-		particle->position.x = particle_index%amount.y;
-		particle->position.y = particle_index/amount.x;
-		particle->position.x*= window.getSize().x/amount.x;
-		particle->position.y*= window.getSize().y/amount.y;
+		particle->position.x = particle_index%amount.y+1;
+		particle->position.y = particle_index/amount.x+1;
+		particle->position.x*= window.getSize().x/(amount.x+1);
+		particle->position.y*= window.getSize().y/(amount.y+1);
 		particle->mass = 10000.f;
 		particle->gravity_exert=true;
 		particle->gravity_affected=true;
@@ -81,6 +98,12 @@ int main()
 				case sf::Keyboard::G:
 					engine.gravity = !engine.gravity;
 					break;
+				case sf::Keyboard::Q:
+					engine.use_quadtree = !engine.use_quadtree;
+					break;
+				case sf::Keyboard::R:
+					render_quadtree = !render_quadtree;
+					break;
 				default:
 					break;
 				}
@@ -89,9 +112,11 @@ int main()
 			}
 		}
 
-		window.clear();
+		window.clear(sf::Color::Black);
 
 		engine.simulate(wind::Engine::StandardDuration(30.f));
+
+		if(render_quadtree)draw_quadtree(window, quadtree.get());
 
 		for(auto& particle : particles)
 		{
@@ -100,5 +125,34 @@ int main()
 		}
 		window.display();
 
+	}
+}
+
+void draw_quadtree(sf::RenderWindow &window, const wind::QuadTree *tree)
+{
+	if(tree->get_nodes()!=nullptr)
+	{
+		draw_quadtree(window, &(tree->get_nodes()[0]));
+		draw_quadtree(window, &(tree->get_nodes()[1]));
+		draw_quadtree(window, &(tree->get_nodes()[2]));
+		draw_quadtree(window, &(tree->get_nodes()[3]));
+	}
+	else
+	{
+		sf::RectangleShape shape;
+		shape.setFillColor({0,0,0,0});
+		if(tree->get_entities().empty())
+		{
+			shape.setOutlineColor({255,0,0, 127});
+			shape.setOutlineThickness(2);
+		}
+		else
+		{
+			shape.setOutlineColor({0,255,0, 127});
+			shape.setOutlineThickness(2);
+		}
+		shape.setPosition({tree->get_bounds().center.x-tree->get_bounds().half_size.x+2.f, tree->get_bounds().center.y-tree->get_bounds().half_size.y+2.f});
+		shape.setSize({tree->get_bounds().half_size.x*2.f-2.f,tree->get_bounds().half_size.y*2.f-2.f});
+		window.draw(shape);
 	}
 }
